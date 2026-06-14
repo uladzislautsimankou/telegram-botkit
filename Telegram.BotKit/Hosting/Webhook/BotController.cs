9e055a7e701ee0c6b75c7069ee48b3430978046d
Uladzislau Tsimankou <99716675+uladzislautsimankou@users.noreply.github.com>
2026-06-14 20:02:01 +0300

    fix(host): pass scoped bot to webhook handler
    Fixes issue where ITelegramBotClient could not be
    used directly inside the update handler.

--- Telegram.BotKit/Hosting/Webhook/BotController.cs
+++ Telegram.BotKit/Hosting/Webhook/BotController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 
@@ -6,14 +7,12 @@ namespace Telegram.BotKit.Hosting.Webhook;
 
 [ApiController]
 [Route("api/bot")]
-public class BotController(IUpdateHandler updateHandler) : ControllerBase
+public class BotController(IUpdateHandler updateHandler, ITelegramBotClient bot) : ControllerBase
 {
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] Update update, CancellationToken ct)
     {
-        // null в качестве botClient, т.к. обычно IUpdateHandler из либы Telegram.Bot ожидает его,
-        // но мы инжектим botClient прямо в хендлеры.
-        await updateHandler.HandleUpdateAsync(null!, update, ct);
+        await updateHandler.HandleUpdateAsync(bot, update, ct);
 
         return Ok();
     }
